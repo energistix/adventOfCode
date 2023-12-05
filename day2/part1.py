29ce98ef44c3069ab440b0d2e908946e0c50e058
Florian Chasseloup <50068022+energistix@users.noreply.github.com>
2023-12-05 08:00:38 +0100

    day 2 part 1

---
+++ day2/part1.py
@@ -0,0 +1,71 @@
+lines: list[str] = []
+with open("./day2/input.txt", "r") as f:
+    lines = f.readlines()
+
+
+colorsMaxs = {"red": 12, "green": 13, "blue": 14}
+colorsList = list(colorsMaxs.keys())
+
+
+def parsInt(string: str):
+    res = ""
+    for char in string:
+        if char.isdigit():
+            res += char
+        else:
+            break
+    return res
+
+
+def parseGame(string: str):
+    balloons: dict[str, int] = {}
+    while not string.startswith(";") and len(string) > 0:
+        color, ammount, string = parseBalloon(string)
+        balloons[color] = ammount
+
+        if string.startswith(","):
+            string = string.replace(",", "", 1)
+    string = string.replace(";", "", 1)
+    return balloons, string
+
+
+def parseColor(string: str):
+    validColors = list(filter(lambda s: string.startswith(s), colorsList))
+    if len(validColors) == 1:
+        return validColors[0]
+    raise ValueError(f"There should be a color at the start of {string}")
+
+
+def parseBalloon(string: str) -> tuple[str, int, str]:
+    ammount = parsInt(string)
+    string = string.replace(ammount, "", 1)
+    color = parseColor(string)
+    string = string.replace(color, "", 1)
+
+    return color, int(ammount), string
+
+
+total: int = 0
+
+for line in lines:
+    line = line.replace("Game", "", 1)
+    line = line.replace(" ", "")
+    line = line.replace("\n", "")
+    gameid = parsInt(line)
+    line = line.replace(f"{gameid}:", "", 1)
+
+    possible = True
+
+    while len(line) > 0 and possible:
+        game, line = parseGame(line)
+        for color, max in colorsMaxs.items():
+            if color in game and game[color] > max:
+                possible = False
+                break
+
+    if possible:
+        total += int(gameid)
+
+    print(f"{gameid} : {line}")
+
+print(total)
